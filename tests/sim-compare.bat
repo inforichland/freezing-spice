a2d8b9365be622617286c209961063521cb6fa06
Tim Wawrzynczak <inforichland@gmail.com>
2015-02-28 17:15:59 -0600

    Added src directory

--- tests/sim-compare.bat
+++ tests/sim-compare.bat
@@ -1,7 +1,7 @@
 @echo off
 
 del *.cf *.vcd
-ghdl -a common.vhd decode_pkg.vhd compare_unit.vhd tests/compare_tb.vhd
+ghdl -a ../src/common.vhd ../src/decode_pkg.vhd ../src/compare_unit.vhd compare_tb.vhd
 ghdl -e compare_tb
 ghdl -r compare_tb --vcd=compare_tb.vcd
 
