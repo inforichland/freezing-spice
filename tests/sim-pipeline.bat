dde32302e57bbfbd6badb4b6b8d0954b07804cf6
Tim Wawrzynczak <inforichland@gmail.com>
2015-07-18 16:23:44 -0600

    Removed extraneous simulation script

--- tests/sim-pipeline.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-del *.cf *.vcd
-ghdl -a ../src/common.vhd ../src/decode_pkg.vhd ../src/decode.vhd ../src/encode_pkg.vhd ../src/regfile.vhd ../src/alu.vhd ../src/compare_unit.vhd ../src/pipeline.vhd pipeline_tb.vhd
-ghdl -e pipeline_tb
-ghdl -r pipeline_tb --stack-size=500000000 --vcd=pipeline_tb.vcd
-
-REM gtkwave decoder_tb.vcd
